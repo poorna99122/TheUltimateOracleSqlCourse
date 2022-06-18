d1231d2dc9911d16293e6836564a195b52c9b280
poorna99122 <poornachandrarao99122@gmail.com>
2022-06-17 17:09:20 -0700

    Environment setup is done

---
+++ 17thJune.sql
@@ -0,0 +1,17 @@
+select * from dual;
+select * from users;
+
+
+SELECT * FROM users;
+
+--ssf
+--SELECT * FROM table
+
+--ct
+--CREATE TABLE table (); 
+
+--ii
+--INSERT INTO table VALUES ()
+
+--df
+--DELETE FROM table WHERE 
