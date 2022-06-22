c238e39d795177bb23ae1a5f7fa5d9ce14c2bf1d
poorna99122 <poornachandrarao99122@gmail.com>
2022-06-21 19:19:12 -0700

    Practice

---
+++ 21stJune.sql
@@ -0,0 +1,14 @@
+-------------------------------------------------
+--24. Practice challenge : basic select statement
+-------------------------------------------------
+ SELECT *
+FROM employee;
+
+SELECT name, phone, salary
+FROM employee;
+
+SELECT *
+FROM department;
+
+SELECT id, name, salary, salary * 1.2
+FROM employee;
