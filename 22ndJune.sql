dbd67c0881a636dc629f035c79437e705248e347
poorna99122 <poornachandrarao99122@gmail.com>
2022-06-22 21:25:25 -0700

    30. Practicing challenge (Filtering results)

--- 22ndJune.sql
+++ 22ndJune.sql
@@ -2,3 +2,42 @@
 --28.Introduction to filtering and sorting results from Oracle SQL Queries.
 ---------------------------------------------------------------------------
 
+--From this section we are going to learn filtering and sorting 
+
+
+---------------------------------------------------------------------------
+--29.Filtering results (Introducing WHERE clause)
+---------------------------------------------------------------------------
+select * from products;
+select * from products where price < 10;
+
+select * from products where expiration_date < '01-jun-2026';
+select * from products where expiration_date < DATE '2026-05-01';
+
+
+---------------------------------------------------------------------------
+-- 30. Practicing Challenge (Filtering results)
+---------------------------------------------------------------------------
+select * from department;
+select * from department where monthly_budget >= 20000;
+
+
+select * from employee;
+select NAME,PHONE,BIRTHDATE from employee where BIRTHDATE < '01-01-1990';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
